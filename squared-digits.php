1b8d8afbff2ee1a04044f9bfe74cff55fde7ade0
Chris McGee <chris@chrismcgee.info>
2022-02-09 11:22:31 -0500

    🎨 Prettifies the code

--- squared-digits.php
+++ squared-digits.php
@@ -3,12 +3,16 @@
 function squared_digits(int $input): int
 {
     // Convert the number input into an array of its constituent digits
-    $digits = array_map('intval', str_split($input));
+    $digits = array_map("intval", str_split($input));
 
     // Square each digit and add them all together
-    return array_reduce($digits, function ($acc, $item) {
-        return $acc + $item ** 2;
-    }, 0);
+    return array_reduce(
+        $digits,
+        function ($acc, $item) {
+            return $acc + $item ** 2;
+        },
+        0
+    );
 }
 
 var_dump(squared_digits(23));
